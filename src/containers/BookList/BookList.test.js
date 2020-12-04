47f1351283873eb57876e1e6e81f94c21c0a3dac
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-04 13:18:23 -0600

    Implement BookList component.

--- src/containers/BookList/BookList.test.js
+++ src/containers/BookList/BookList.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import BookList from './BookList';
 
-describe('App', () => {
+describe('BookList', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<BookList />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('Has a valid snapshot', () => {
-    const component = renderer.create(<App />);
+    const component = renderer.create(<BookList />);
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
