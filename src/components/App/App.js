e5cbda1b93fac261a84872e7a3354e7734c7e6a3
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-04 13:12:28 -0600

    Implement App component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 
 const App = () => (
   <div>
-    Book store app.
+    <BookForm />
+    <BookList />
   </div>
 );
 
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,19 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Book store app./i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("Has a valid snapshot", () => {
+    const component = renderer.create(<App />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
