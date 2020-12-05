4b6b530f28c26f6c9a86634f088d89f9a64edfe3
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-04 20:12:16 -0600

    Connect BookList with the redux store.

--- src/containers/BookList/index.js
+++ src/containers/BookList/index.js
@@ -1,1 +1,6 @@
-export { default } from './BookList';
+import { connect } from 'react-redux';
+import BookList from './BookList';
+
+const mapStateToProps = state => ({ books: state.books.length > 0 ? state.books : undefined });
+
+export default connect(mapStateToProps)(BookList);
