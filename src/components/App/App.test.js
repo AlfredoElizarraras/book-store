d114233a46fe8d984f26beddec4892831697953b
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-05 01:23:55 -0600

    Add the store to the app test.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from '../../store';
 
 describe('App', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('Has a valid snapshot', () => {
-    const component = renderer.create(<App />);
+    const component = renderer.create(<Provider store={store}><App /></Provider>);
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
