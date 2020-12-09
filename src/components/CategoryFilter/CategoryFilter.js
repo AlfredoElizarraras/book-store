266fb73d876c8ea72dff569c4e08f10432ba7aeb
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-08 20:48:09 -0600

    Fix linter errors.

--- src/components/CategoryFilter/CategoryFilter.js
+++ src/components/CategoryFilter/CategoryFilter.js
@@ -1,19 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import { v4 as uuid } from 'uuid';
-import BOOK_CATEGORIES from "../../utils/const";
+import BOOK_CATEGORIES from '../../utils/const';
 
-const CategoryFilter = (handleFilterChange) => (
+const CategoryFilter = handleFilterChange => (
   <>
-  <label htmlFor="categories">Fiter by category</label>
-  <select id="categories" name="categories" onChange={e => handleFilterChange(e.target.value)}>
-    <option>ALL</option>
-    {BOOK_CATEGORIES.map((category) => (
-      <option key={uuid()} value={category}>
-        {category.toUpperCase()}
-      </option>
-    ))}
-  </select>
+    <label htmlFor="categories">
+      Fiter by category
+      <select id="categories" type="categories" name="categories" onChange={e => handleFilterChange(e.target.value)}>
+        <option>ALL</option>
+        {BOOK_CATEGORIES.map(category => (
+          <option key={uuid()} value={category}>
+            {category.toUpperCase()}
+          </option>
+        ))}
+      </select>
+    </label>
   </>
 );
 
