cf821ea0d6fcb4561a6e8d70767dbe9efc98e300
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-11 15:06:54 -0600

    Add new data placeholders and example data based on design.

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -9,15 +9,9 @@ const Book = ({ book, handleRemoveBook }) => (
   <article id={book.id} className="flex book jc-sb">
     <div>
       <div>
-        <span className="book__main-info__category">
-          {book.category}
-        </span>
-        <h2 className="book__main-info__title">
-          {book.title}
-        </h2>
-        <span className="book__main-info__author">
-          book author
-        </span>
+        <span className="book__main-info__category">{book.category}</span>
+        <h2 className="book__main-info__title">{book.title}</h2>
+        <span className="book__main-info__author">{book.author}</span>
       </div>
       <div className="book__action-links">
         <a href="#the-hunger-games" className="book__action-link">
@@ -30,19 +24,34 @@ const Book = ({ book, handleRemoveBook }) => (
         >
           Remove
         </a>
-        <a href="#the-hunger-games" className="book__action-link book__action-link--last">
+        <a
+          href="#the-hunger-games"
+          className="book__action-link book__action-link--last"
+        >
           Edit
         </a>
       </div>
     </div>
     <div className="flex jc-sb">
       <div className="flex jc-c ai-c book__chart-wrapper">
-        <CircleProgressChart percent={book.progress} radius="35" strokeWidth="5" />
+        <CircleProgressChart
+          percent={book.progress}
+          radius="35"
+          strokeWidth="5"
+          maxValue={book.maxProgressValue}
+          currentValue={book.currentProgressValue}
+        />
         <span className="book__chart-wrapper--border-right" />
       </div>
       <div className="flex flex-col">
-        <span className="book__progress-measure">Current book progress measure</span>
-        <span className="book__current-progress-value">progress value</span>
+        <span className="book__progress-measure">
+          Current
+          {' '}
+          {book.progressMeasure}
+        </span>
+        <span className="book__current-progress-value">
+          {book.progressMeasureValue}
+        </span>
         <Button margin="0.6rem 0px 0px">Update progress</Button>
       </div>
     </div>
@@ -55,6 +64,11 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
     progress: PropTypes.number,
+    author: PropTypes.string,
+    progressMeasure: PropTypes.string,
+    progressMeasureValue: PropTypes.string,
+    maxProgressValue: PropTypes.number,
+    currentProgressValue: PropTypes.number,
   }),
   handleRemoveBook: PropTypes.func.isRequired,
 };
@@ -62,6 +76,11 @@ Book.propTypes = {
 Book.defaultProps = {
   book: {
     progress: 30,
+    author: 'Book author',
+    progressMeasure: 'chapter',
+    progressMeasureValue: 'Introduction',
+    maxProgressValue: 0,
+    currentProgressValue: 0,
   },
 };
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -6,6 +6,11 @@ export default [
     title: 'The Hunger Games',
     category: 'Action',
     progress: 64,
+    author: 'Suzanne Collins',
+    progressMeasureValue: 'Chapter 17',
+    progressMeasure: 'chapter',
+    maxProgressValue: 0,
+    currentProgressValue: 0,
   },
 
   {
@@ -13,11 +18,22 @@ export default [
     title: 'Dune',
     category: 'Science Fiction',
     progress: 8,
+    author: 'Frank Herbert',
+    progressMeasureValue: 'Chapter 3: "A Lesson Learned"',
+    progressMeasure: 'chapter',
+    maxProgressValue: 0,
+    currentProgressValue: 0,
   },
+
   {
     id: uuid(),
     title: 'Capital in the Twenty-First Century',
     category: 'Economy',
     progress: 0,
+    author: 'Suzanne Collins',
+    progressMeasure: 'chapter',
+    progressMeasureValue: 'Introduction',
+    maxProgressValue: 0,
+    currentProgressValue: 0,
   },
 ];
