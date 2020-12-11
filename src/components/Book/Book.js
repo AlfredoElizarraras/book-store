c65cf58454e17bd29c591912e24eb2486bee0915
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-12-11 02:22:01 -0600

    Complete book main info.

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -7,22 +7,39 @@ const Book = ({ book, handleRemoveBook }) => (
   <article id={book.id} className="flex book jc-sb">
     <div>
       <div>
-        <span>{book.category}</span>
-        <h2>{book.title}</h2>
-        <span>book author</span>
+        <span className="book__main-info__category">
+          {book.category}
+        </span>
+        <h2 className="book__main-info__title">
+          {book.title}
+        </h2>
+        <span className="book__main-info__author">
+          book author
+        </span>
       </div>
-      <div>
-        <a href="#the-hunger-games">Comments</a>
-        <a href="#the-hunger-games" onClick={() => handleRemoveBook(book.id)}>Remove</a>
-        <a href="#the-hunger-games">Edit</a>
+      <div className="book__action-links">
+        <a href="#the-hunger-games" className="book__action-link">
+          Comments
+        </a>
+        <a
+          href="#the-hunger-games"
+          className="book__action-link"
+          onClick={() => handleRemoveBook(book.id)}
+        >
+          Remove
+        </a>
+        <a href="#the-hunger-games" className="book__action-link book__action-link--last">
+          Edit
+        </a>
       </div>
     </div>
-    <div className="flex jc-c ai-c">
-      Book visual progress
-    </div>
-    <div className="flex flex-col">
-      <span>Current book progress measure</span>
-      <button type="button">Update progress</button>
+    <div className="flex jc-sb">
+      <div className="flex jc-c ai-c">Book visual progress</div>
+      <div className="flex flex-col">
+        <span>Current book progress measure</span>
+        <span>progress value</span>
+        <button type="button">Update progress</button>
+      </div>
     </div>
   </article>
 );
