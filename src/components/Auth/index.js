c70ac565d9540394ed7cfcef7384252a1a9337a6
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-05 17:03:52 -0600

    Add Auth component to be the router component.

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,1 +1,37 @@
-export { default } from './Login';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Login from './Login';
+import App from '../App';
+
+const isAuthenticated = false;
+
+const Auth = () => (
+  <Router>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/sign-up">
+        <Login type="signup" />
+      </Route>
+      <Route
+        exact
+        path="/"
+        render={() => (
+
+          isAuthenticated
+            ? <App />
+            : <Redirect to="/login" />
+
+        )}
+      />
+    </Switch>
+  </Router>
+);
+
+export default Auth;
