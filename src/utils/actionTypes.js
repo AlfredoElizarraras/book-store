8b680d2c5523992cdb80acb8838ca0846a6e0c4e
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-07 16:28:40 -0600

    Add User action types.

--- src/utils/actionTypes.js
+++ src/utils/actionTypes.js
@@ -1,3 +1,5 @@
 export const ADD_BOOK = 'ADD_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
 export const CHANGE_FILTER = 'CHANGE_FILTER';
+export const USER_LOGIN = 'USER_LOGIN';
+export const USER_LOGOUT = 'USER_LOGOUT';
